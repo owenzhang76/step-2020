28b83a0b9266f16339bb5fbf1107aa1d66aaf111
Owen Zhang <owenzhang76@gmail.com>
2020-05-23 05:12:30 -0700

    added iframes, carousel, dark-mode section

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,20 +1,5 @@
 // Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
 let elementsToShow;
 let scroll;
 
@@ -22,22 +7,24 @@ $(document).ready(function() {
     scroll = window.requestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60)};
     elementsToShow = document.querySelectorAll('.animate');    
     loop();
-    removeImagePlaceHolder();
+    asyncRemovePlaceHolder();
 })
 
 function addRandomGreeting() {
+
     revertToUnchecked();
+
     const greetings =
-        ["In a world too often governed by corruption and arrogance, it can be difficult to stay true to one's philosophical and literary principles.   -Lemony Snicket, A Series of Unfortunate Events", "Did you know that the Sparrow flies south for winter?   -Skulduggery Pleasant, Skulduggery Pleasant", "Someday in your life, you'll realize there is no turning back. There was never any turning back.   -u/JayStar1213"];
+        ["\"In a world too often governed by corruption and arrogance, it can be difficult to stay true to one's philosophical and literary principles.\"   -Lemony Snicket, A Series of Unfortunate Events", "\"Did you know that the Sparrow flies south for winter?\"   -Skulduggery Pleasant, Skulduggery Pleasant", "\"Someday in your life, you'll realize there is no turning back. There was never any turning back.\"   -u/JayStar1213"];
     const greeting = greetings[Math.floor(Math.random() * greetings.length)];
   
     let entrances = 
         ["animate__bounceInUp", "animate__bounceInLeft", "animate__bounceInRight", "animate__bounceInDown", "animate__fadeInBottomLeft", "animate__fadeInBottomRight"]
     let entrance = entrances[Math.floor(Math.random() * greetings.length)];
-    // Add it to the page.
-    const quoteContainer = document.getElementById('quote-container');
+
     const quote = document.getElementById('quote');
     quote.className="";
+    quote.classList.add('italic');
     quote.classList.add('animate__animated');
     quote.classList.add(entrance);
     quote.classList.add('animate__delay-1s');
@@ -45,32 +32,49 @@ function addRandomGreeting() {
    
 }
 
+function asyncRemovePlaceHolder() {
+    setTimeout(function(){ 
+        $('#placeholder').remove(); 
+    }, 2000);
+}
+
 function revertToUnchecked() {
+    let label = document.getElementById('quote-label');
+    label.className= "";
+    label.classList= "";
+    label.classList.remove('animate__animated');
+    label.classList.remove('animate__fadeInDown');
+    
     setTimeout(function(){ document.getElementById('btnControl').click(); }, 500);
-}
 
-function removeImagePlaceHolder() {
-    setTimeout(function(){ $('#img-placeholder').remove(); }, 3000);
 }
-
 
 function loop() {
-    // console.log('inside loop')
-    // console.log(elementsToShow);
-    // console.log('inside loop');
-    console.log(elementsToShow);
     elementsToShow.forEach(function (element) {
       if (isElementInViewport(element)) {
-        if(element.classList.contains('blog-images-row')) {
-            element.classList.add('animate__animated');
-            element.classList.add('animate__fadeIn');
+        element.classList.add('animate__animated');
+        if (element.classList.contains('blog-images-row')) {
             element.classList.add('animate__delay-1s');
-        } else if(element.classList.contains('1s-delay')) {
-            element.classList.add('animate__animated');
+            if (element.classList.contains('image-row-left')) {
+                element.classList.add('animate__fadeInLeft');
+            } else if (element.classList.contains('image-row-right')) {
+                element.classList.add('animate__fadeInRight');
+            } else {
+                element.classList.add('animate__fadeInUp');
+            }
+        } else if (element.classList.contains('delay')) {
             element.classList.add('animate__fadeInDown');
-            element.classList.add('animate__delay-1s');
-        } else {
-            element.classList.add('animate__animated');
+            if (element.classList.contains('1s-delay')) {
+                element.classList.add('animate__delay-1s');
+            } else if (element.classList.contains('2s-delay')) {
+                element.classList.add('animate__delay-2s');
+            } else {
+                element.classList.add('animate__delay-3s');
+            }
+        } else if (element.classList.contains('video-container')) {
+            element.classList.add('animate__fadeInUp');
+        }
+        else {
             element.classList.add('animate__fadeInDown');
         }
       }
