1e9599fb4b98dd065569502324ba01beeaf46da6
Owen Zhang <owenzhang76@gmail.com>
2020-06-02 02:27:13 +0000

    add hard code comments

--- portfolio/src/main/webapp/forum.js
+++ portfolio/src/main/webapp/forum.js
@@ -2,13 +2,15 @@
 // Include: Creating a DOM Object
 // Include: Adding to DOM Tree
 
-function displayQuote() {
-    fetch('/randomQuote')
-        .then(response => response.text)
-        .then((quote) => {
-            let quoteDiv = document.createElement('div');
-            quoteDiv.classList.add("quote");
-            quoteDiv.innerHTML = quote;
-            document.getElementById("comments-container").appendChild(quoteDiv);
+function displayComment() {
+    console.log("displayComment ran");
+    fetch('/add-comment')
+        .then(response => response.text())
+        .then((comment) => {
+            let commentDiv = document.createElement('div');
+            commentDiv.classList.add("comment");
+            commentDiv.innerHTML = comment;
+            document.getElementById("comments-container").appendChild(commentDiv);
         })
+        .catch(err => console.log(err));
 }
