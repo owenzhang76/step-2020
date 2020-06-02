39e3a98bf1b82aaaaecbbd23e0ddb09bec215ac6
Owen Zhang <owenzhang76@gmail.com>
2020-06-02 22:24:07 +0000

    add add comments functionality

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -32,24 +32,28 @@ public class DataServlet extends HttpServlet {
     @Override
     public void init() {
         this.messages = new ArrayList<String>();
-        this.messages.add("What is my purpose in life?");
-        this.messages.add("Why can't I stay happy?");
-        this.messages.add("Who lives in a pinneapple under the sea?");
+        // this.messages.add("What is my purpose in life?");
+        // this.messages.add("Why can't I stay happy?");
+        // this.messages.add("Who lives in a pinneapple under the sea?");
     }
-
-    // public DataServlet () {
-    //     messages = new Arraylist<>();
-    //     messages.add("What is my purpose in life?");
-    //     messages.add("Why can't I stay happy?");
-    //     messages.add("Who lives in a pinneapple under the sea?");
-    // }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("doGet ran");
+        System.out.println("messages array size: " + messages.size());
         String messagesJson = convertToJsonUsingGson(messages);
         response.setContentType("application/json");
         response.getWriter().println(messagesJson);
         System.out.println(messagesJson);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("doPost ran");
+        String comment = getParameter(request, "comment-input", "");
+        System.out.println("comment is: " + comment);
+        messages.add(comment);
+        response.sendRedirect("/forum.html");
     }
 
     private String convertToJsonUsingGson(ArrayList messages) {
@@ -57,4 +61,12 @@ public class DataServlet extends HttpServlet {
         String json = gson.toJson(messages);
         return json;
     }
+
+    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
 }
--- portfolio/src/main/webapp/forum.js
+++ portfolio/src/main/webapp/forum.js
@@ -2,30 +2,30 @@
 // Include: Creating a DOM Object
 // Include: Adding to DOM Tree
 
-function displayComment() {
-    console.log("displayComment ran");
-    fetch('/add-comment')
-        .then(response => response.text())
-        .then((comment) => {
-            let commentDiv = document.createElement('div');
-            commentDiv.classList.add("comment");
-            commentDiv.innerHTML = comment;
-            document.getElementById("comments-container").appendChild(commentDiv);
-        })
-        .catch(err => console.log(err));
-}
+// function displayComment() {
+//     console.log("displayComment ran");
+//     fetch('/add-comment')
+//         .then(response => response.text())
+//         .then((comment) => {
+//             let commentDiv = document.createElement('div');
+//             commentDiv.classList.add("comment");
+//             commentDiv.innerHTML = comment;
+//             document.getElementById("comments-container").appendChild(commentDiv);
+//         })
+//         .catch(err => console.log(err));
+// }
 
-function displayThoughts() {
-    console.log('displayThoughts ran');
+function displayComments() {
+    console.log('displayComments ran');
     fetch('/add-comment')
         .then(response => response.json())
-        .then((thoughts) => {
-            console.log(thoughts);
-            for (var index in thoughts) {
-                let thoughtDiv = document.createElement('div');
-                thoughtDiv.classList.add("thought");
-                thoughtDiv.innerHTML = thoughts[index];
-                document.getElementById("comments-container").appendChild(thoughtDiv);
+        .then((comments) => {
+            console.log(comments);
+            for (var index in comments) {
+                let commentDiv = document.createElement('div');
+                commentDiv.classList.add("thought");
+                commentDiv.innerHTML = comments[index];
+                document.getElementById("comments-container").appendChild(commentDiv);
             }
         })
         .catch(err => console.log(err));
